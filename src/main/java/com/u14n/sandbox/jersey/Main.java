2dece0366368edb837715192266c5f4af76ea712
Klaus Wenger <kjwenger@yahoo.com>
2015-11-14 17:48:39 -0500

    extracted JAX-RS common code providing for jersey testing

--- src/main/java/com/u14n/sandbox/jersey/Main.java
+++ src/main/java/com/u14n/sandbox/jersey/Main.java
@@ -12,34 +12,40 @@ import java.net.URI;
  *
  */
 public class Main {
-    // Base URI the Grizzly HTTP server will listen on
-    public static final String BASE_URI = "http://localhost:8080/rest/";
+	// Base URI the Grizzly HTTP server will listen on
+	public static final String BASE_URI = "http://localhost:8081/rest/";
 
-    /**
-     * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
-     * @return Grizzly HTTP server.
-     */
-    public static HttpServer startServer() {
-        // create a resource config that scans for JAX-RS resources and providers
-        // in com.u14n.sandbox.jersey package
-        final ResourceConfig rc = new ResourceConfig().packages("com.u14n.sandbox.jersey");
+	/**
+	 * Starts Grizzly HTTP server exposing JAX-RS resources defined in this
+	 * application.
+	 * 
+	 * @return Grizzly HTTP server.
+	 */
+	public static HttpServer startServer() {
+		// create a resource config that scans for JAX-RS resources and
+		// providers
+		// in com.u14n.sandbox.jersey package
+		final ResourceConfig rc =
+				new ResourceConfig().packages("com.u14n.sandbox.jaxrs");
 
-        // create and start a new instance of grizzly http server
-        // exposing the Jersey application at BASE_URI
-        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
-    }
+		// create and start a new instance of grizzly http server
+		// exposing the Jersey application at BASE_URI
+		return GrizzlyHttpServerFactory.createHttpServer(
+				URI.create(BASE_URI), rc);
+	}
 
-    /**
-     * Main method.
-     * @param args
-     * @throws IOException
-     */
-    public static void main(String[] args) throws IOException {
-        final HttpServer server = startServer();
-        System.out.println(String.format("Jersey app started with WADL available at "
-                + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
-        System.in.read();
-        server.shutdownNow();
-    }
+	/**
+	 * Main method.
+	 * 
+	 * @param args
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws IOException {
+		final HttpServer server = startServer();
+		System.out.println(String.format(
+				"Jersey app started with WADL available at " + "%sapplication.wadl\nHit enter to stop it...",
+				BASE_URI));
+		System.in.read();
+		server.shutdownNow();
+	}
 }
-
